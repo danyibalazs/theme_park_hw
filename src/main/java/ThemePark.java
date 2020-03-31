73f81a49e4b37e7edefa682645a81fa4effe6b28
danyibalazs <danyi.balage@gmail.com>
2020-03-31 19:14:49 +0100

    finish extensions

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,31 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import people.Visitor;
+import stalls.Stall;
+
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private String name;
+
+    public ThemePark(String name){
+        this.name = name;
+        this.attractions = new ArrayList<>();
+        this.stalls = new ArrayList<>();
+    }
+
+    public ArrayList<IReviewed> getAllReviewed() {
+        ArrayList<IReviewed> allReviewed = new ArrayList<IReviewed>();
+        allReviewed.addAll(attractions);
+        allReviewed.addAll(stalls);
+        return allReviewed;
+    }
+
+    public void visit(Visitor visitor, Attraction attraction) {
+        visitor.addAttraction(attraction);
+        attraction.incrementVisitCount();
+    }
 }
